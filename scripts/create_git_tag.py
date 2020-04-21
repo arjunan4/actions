f2aa212396cc3887805ee7c0d33df656f7f113a0
arjunanashok <arjunan.ashok@gmail.com>
2020-04-21 13:18:10 +0100

    qwsdfvbn

--- scripts/create_git_tag.py
+++ scripts/create_git_tag.py
@@ -6,8 +6,8 @@ class CreateGitTags:
 
     def create_git_tag(self):
         print('Creating Git tag for the repository after PR is merged')
-        # g = git.cmd.Git()
-        # print("Git First command is  " + g)
+        g = git.cmd.Git()
+        print("Git First command is  " + g)
 
 
 hello = CreateGitTags()
