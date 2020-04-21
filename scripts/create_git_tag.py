49944959b6ec416fe885c7ac6a047c697456238f
arjunanashok <arjunan.ashok@gmail.com>
2020-04-21 12:20:47 +0100

    first python file being added

---
+++ scripts/create_git_tag.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+class CreateGitTags:
+
+    def create_git_tag(self):
+        print('Creating Git tag for the repository after PR is merged')
+
+
+hello = CreateGitTags()
+hello.create_git_tag()
