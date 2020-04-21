09054433c3c9aeddb633deb7df1796c39cc899e2
arjunanashok <arjunan.ashok@gmail.com>
2020-04-21 20:25:47 +0100

    Final Commit for git tag version

--- scripts/create_git_tag.py
+++ scripts/create_git_tag.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import git
 from git import Repo
 import os
 
@@ -8,17 +7,22 @@ class CreateGitTags:
 
     def create_git_tag(self):
         os.chdir('../')
-        print("Changing to home directory", os.getcwd())
-        print('List directories', os.listdir())
         repo = Repo(os.getcwd())
-        # print("Git Repository Head details ==> ", repo.head.ref)
-        # print("Git Repository Master details ==>", repo.heads.master)
-        print('Fetching remote tags===>')
-        print(repo.remote().fetch('--tags'))
-        print("Git Repository tags details ==>", repo.tags)
-        tag_result = repo.git.tag(l=True)
-        print('Tags results Object Type ==>', type(tag_result))
-        print('Tags results are ==>', tag_result)
+        print('===> Fetching remote tags===>')
+        repo.remote().fetch('--tags')
+        result = repo.git.tag(l=True)
+        tag_list = result.strip().split('\n')
+        print("Git Tag results, before sorting in Descending Order => ", tag_list)
+        range_number = len(tag_list)
+        print("Number of elements in the list =>", range_number)
+        for i in range(range_number):
+            for j in range(i + 1, range_number):
+                if tag_list[i] < tag_list[j]:
+                    temp = tag_list[i]
+                    tag_list[i] = tag_list[j]
+                    tag_list[j] = temp
+        print("Git Tag results, after sorting in Descending Order => ", tag_list)
+        print("Required Git tag version", tag_list[0])
         print("Changing Directory to Original Path")
         os.chdir('./scripts')
         print("Changing to python script directories", os.getcwd())
