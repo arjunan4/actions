a429cb8e3bc52e1c425a1092823d7edf6bf6bc70
arjunanashok <arjunan.ashok@gmail.com>
2020-04-22 20:51:15 +0100

    afdsfssfsf

--- scripts/create_git_tag.sh
+++ scripts/create_git_tag.sh
@@ -16,31 +16,51 @@ function error {
 # get latest tag
 git fetch --all --tags
 tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-info "Git Latest Tag is ==> $tag"
 
-if [ -z "$tag" ]
-then
-    log=$(git log --pretty=oneline)
-    tag=0.0.0
-else
-    log=$(git log $t..HEAD --pretty=oneline)
-fi
-
-info "Git Tag exists for this repository ==> $tag"
-info "Git Log this repository ==> $log"
-
-if [ -n $tag ]; then
+info "Git Tag ==> $tag"
+if [ -n "$tag" ]; then
     info "Git Tag exists for this repository ==> $tag"
-    new_version=$(`semver bump najor $tag`)
-    info "New Git Tag is ==>  $new_version"
 else
     info "No Git Tag found for this repository"
+    tag="0.0.0"
 fi
 
-case "$log" in
-    *#major* ) new=$(`semver bump major $tag`);;
-    *#patch* ) new=$(`semver bump patch $tag`);;
-    * ) new=$(`semver bump major $tag`);;
-esac
+IFS='.'
+read -ra ADDR <<< "$tag"
 
-info "After case statement ==> $tag"
+info "Existing Tag version details => $tag" 
+info "Git Tag is splitted by . and array length is ==> ${#ADDR[@]}"
+
+if [ ${#ADDR[@]} = 3 ]; then
+  current_major_ver=${ADDR[0]}
+  current_minor_ver=${ADDR[1]}
+  current_patch_ver=${ADDR[2]}
+  if [[ ${ADDR[0]} == *"v"* ]];then
+    current_major_ver=${ADDR[0]#"v"}
+  fi
+  info "Major => $current_major_ver ; Minor => $current_minor_ver ; Patch => $current_patch_ver"
+  if [ $current_minor_ver = 9 ]; then
+    new_minor_ver="0"
+    new_patch_ver="0"
+    let "new_major_ver=$current_major_ver+1"
+  else
+    let "new_minor_ver=$current_minor_ver + 1"
+    new_patch_ver="0"
+    new_major_ver="$current_major_ver" 
+  fi
+  new_tag_version="v$new_major_ver.$new_minor_ver.$new_patch_ver"
+  new_tag_version=$new_tag_version | sed -e 's/^[[:space:]]*//'
+  info "New Tag version details => $new_tag_version" 
+  info "Major => $new_major_ver ; Minor => $new_minor_ver ; Patch => $new_patch_ver"
+else
+  info "Git tag format is NOT as expcted,hence exiting version script"
+  exit 1
+fi
+
+commit_message="Upgrading Git Tag to $new_tag_version"
+set -e
+git commit -m "Upgrading Git Tag to $new_tag_version"
+# # git tag $new_tag_version
+# # git push -q origin master
+# # git push -q --tag
+set +e
