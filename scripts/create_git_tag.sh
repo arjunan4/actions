68e793f2d83bc46c265f02c73d0bb5f05f590a87
arjunanashok <arjunan.ashok@gmail.com>
2020-04-22 11:38:31 +0100

    afafaa

--- scripts/create_git_tag.sh
+++ scripts/create_git_tag.sh
@@ -21,7 +21,7 @@ info "Git Latest Tag is ==> $tag"
 
 if [ -n $tag ]; then
     info "Git Tag exists for this repository ==> $tag"
-    new_tag = $(semvertag bump minor)
+    new_tag = $(semver bump major $tag)
     info "New Git Tag is ==>  $new_tag"
 else
     info "No Git Tag found for this repository"
