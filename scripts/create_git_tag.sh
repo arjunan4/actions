ce88d1062f8a7c9a344e6dbcab4ef426b3c375ed
arjunan4 <arjunan.ashok@gmail.com>
2020-04-23 16:14:34 +0100

    asfafa

--- scripts/create_git_tag.sh
+++ scripts/create_git_tag.sh
@@ -87,7 +87,7 @@ EOF
 }
 
 
-#posting a curl requestshy
+#posting a curl requestshyff
 curl_response=$(curl -s -X POST $github_repo_url -H "Authorization: token $GITHUB_TOKEN" -d "$(post_data)")
 
 info "Output Refs -> $curl_response"
