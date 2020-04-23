dd841ed57fa54ab7bbadc3923e198c94baccddcc
arjunan4 <arjunan.ashok@gmail.com>
2020-04-23 09:25:19 +0100

    fdfafafasfsa

--- scripts/create_git_tag.sh
+++ scripts/create_git_tag.sh
@@ -121,5 +121,10 @@ info "Git tag_hash $tag_hash"
 
 curl -s -X POST $github_repo_url \
 -H "Authorization: token $GITHUB_TOKEN" \
--d tag_hash
+-d @- << EOF
+{
+  "ref": "refs/tags/$new_tag_version",
+  "sha": "$commit"
+}
+EOF
 info "CURL output $?"
